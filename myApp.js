58eff16e6710c6dabf9c49338ad288f8b538f820
sandaiiyahh <sandydai98@gmail.com>
2022-11-12 22:17:41 -0500

    output hello world string and express server

--- myApp.js
+++ myApp.js
@@ -1,39 +1,10 @@
 let express = require('express');
 let app = express();
 
+console.log('Hello World');
 
+app.get('/', (req, res) => {
+  res.send('Hello Express');
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
