42e16067ef69ca24783709a2c3623b391fdd00e1
sandaiiyahh <sandydai98@gmail.com>
2022-11-12 22:24:15 -0500

    feat: serve HTML file

--- myApp.js
+++ myApp.js
@@ -3,8 +3,14 @@ let app = express();
 
 console.log('Hello World');
 
+// Serve a string using express server:
+// app.get('/', (req, res) => {
+//   res.send('Hello Express');
+// });
+
+// Serve a HTML file:
 app.get('/', (req, res) => {
-  res.send('Hello Express');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 module.exports = app;
